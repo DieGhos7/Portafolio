72691b4bcdb0a5c863abac8e3dd16a541dff3659
Diego Romero <diego.romer70@gmail.com>
2023-10-25 19:09:49 -0300

    actualizacion tareas

--- scriptEscrito.js
+++ scriptEscrito.js
@@ -13,3 +13,28 @@ calcularBoton.addEventListener("click", function () {
     var velocidad = calcularVelocidad(distancia, tiempo);
     velocidadSpan.textContent = velocidad.toFixed(2) + " m/min";
 });
+
+function calcularIVA(dinero) {
+    var iva = parseFloat(dinero) * 0.22;
+    return iva;
+}
+
+function costeTotal(dinero) {
+    var iva = parseFloat(dinero) * 0.22;
+    var costeTotal = parseFloat(dinero) + (iva);
+    return costeTotal;
+}
+
+var calcularBotonIva = document.getElementById("calcularIva");
+var costeTotalSpan = document.getElementById("costeTotal");
+var cantIvaSpan = document.getElementById("cantIva");
+
+calcularBotonIva.addEventListener("click", function () {
+    var dinero = document.getElementById("dinero").value;
+    var iva = calcularIVA(dinero);
+    var coste = costeTotal(dinero);
+    cantIvaSpan.textContent = iva + " UYU";
+    costeTotalSpan.textContent = coste + " UYU";
+});
+
+
